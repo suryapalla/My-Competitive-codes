fdd4b12e664f37a69d657d5c6ae9e9fd4c5122b4
CANDY\pakumar <undefendable.surya@gmail.com>
2018-02-05 16:59:30 +0530

    Simple implementation of hash table where we can store  key value pairs through which we can achieve many problems.

---
+++ Test.java
@@ -0,0 +1,19 @@
+/******
+Simple implementation of hash table where we can store  key value pairs through which we can achieve many problems.
+*******/
+
+import java.util.*;  
+class Child{  
+ public static void main(String args[]){  
+  Hashtable<Integer,String> hm=new Hashtable<Integer,String>();  
+  
+  hm.put(100,"Amit");  
+  hm.put(102,"Ravi");  
+  hm.put(101,"Vijay");  
+  hm.put(103,"Rahul"); 
+  
+  for(Map.Entry m:hm.entrySet()){  
+   System.out.println(m.getKey()+" "+m.getValue());  
+  }  
+ }  
+}  
